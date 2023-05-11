39b9f29ea817d1ba36df1e0fafdfb44f2b191a35
A-Kija <arvydas.kijakauskas@gmail.com>
2023-05-11 14:29:40 +0300

    inputs

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.scss';
+import Inputs from './Components/006/Inputs';
+// import Ls from './Components/006/Ls';
+// import Sq4 from './Components/006/Sq4';
 import './buttons.scss';
-import Buttons from './Components/005/Buttons';
 
 
 function App() {
@@ -8,9 +10,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Sq4 /> */}
 
-        <Buttons />
-
+        {/* <Ls /> */}
+        <Inputs />
       </header>
     </div>
   );
