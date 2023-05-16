ba30bf9c23e88d51dc23e8581e5532da02692286
A-Kija <arvydas.kijakauskas@gmail.com>
2023-05-16 13:58:45 +0300

    crud done

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -3,40 +3,84 @@ import './App.scss';
 import './buttons.scss';
 import Create from './Components/crud/Create';
 import { useEffect, useState } from 'react';
-import { crudCreate, crudRead } from './Functions/localStorageCrud';
+import { crudCreate, crudDelete, crudEdit, crudRead } from './Functions/localStorageCrud';
 import List from './Components/crud/List';
+import Delete from './Components/crud/Delete';
+import Edit from './Components/crud/Edit';
 
 const KEY = 'myFancyColors';
 
 export default function App() {
 
+    const [listUpdate, setListUpdate] = useState(Date.now());
 
     const [colors, setColors] = useState(null);
 
     const [createData, setCreateData] = useState(null);
+    const [deleteModalData, setDeleteModalData] = useState(null);
+    const [deleteData, setDeleteData] = useState(null);
+    const [editModalData, setEditModalData] = useState(null);
+    const [editData, setEditData] = useState(null);
 
+    //R read
     useEffect(_ => {
         setColors(crudRead(KEY));
-    }, []);
+    }, [listUpdate]);
 
+    //C create
     useEffect(_ => {
         if (null === createData) {
             return;
         }
-        crudCreate(KEY, createData)
+        crudCreate(KEY, createData);
+        setListUpdate(Date.now());
     }, [createData]);
 
+    //U update
+    useEffect(_ => {
+        if (null === editData) {
+            return;
+        }
+        crudEdit(KEY, editData, editData.id);
+        setListUpdate(Date.now());
+    }, [editData]);
+
+    //D dreate
+    useEffect(_ => {
+        if (null === deleteData) {
+            return;
+        }
+        crudDelete(KEY, deleteData.id);
+        setListUpdate(Date.now());
+    }, [deleteData]);
+
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-4">
-                    <Create />
-                </div>
-                <div className="col-8">
-                    <List colors={colors} />
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <Create setCreateData={setCreateData} />
+                    </div>
+                    <div className="col-8">
+                        <List
+                            colors={colors}
+                            setDeleteModalData={setDeleteModalData}
+                            setEditModalData={setEditModalData}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+            <Delete
+                deleteModalData={deleteModalData}
+                setDeleteModalData={setDeleteModalData}
+                setDeleteData={setDeleteData}
+            />
+            <Edit
+                editModalData={editModalData}
+                setEditModalData={setEditModalData}
+                setEditData={setEditData}
+            />
+        </>
     );
 
 }
