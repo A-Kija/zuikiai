118d296c5fabb3988da90efb487f22c92c62dee2
A-Kija <arvydas.kijakauskas@gmail.com>
2023-06-12 10:57:27 +0300

    go

--- 022/Ufo.php
+++ 022/Ufo.php
@@ -3,10 +3,53 @@
 
 class Ufo {
 
-    public $speed = 5;
+    public $speed;
+
+    private $destination;
 
 
-    private function whatTime() : string
+
+    public function __construct($where, $speed = 5)
+    {
+        $this->destination = $where;
+        $this->speed = $speed;
+    }
+
+    public function __destruct()
+    {
+        echo '<h1>UFO GONE</h1>';
+    }
+
+
+    public function __get($prop)
+    {
+       
+        return $this->$prop;
+         
+        
+        // if ($prop == 'bebras') {
+        //     return 'Bebro nėra, bet yra Zebras';
+        // }
+
+        // if ($prop == 'destination') {
+        //     return $this->destination;
+        // }
+       
+        // echo "<h1>$prop</h1>";
+    }
+
+
+    public function __set($prop, $what)
+    {
+        if (in_array($what, ['Moon', 'Mars', 'Pluto'])) {
+            $this->$prop = $what;
+        }
+        
+        
+    }
+
+
+    protected function whatTime() : string
     {
         return date('d:s');
     }
--- 022/index.php
+++ 022/index.php
@@ -5,15 +5,15 @@ echo '<pre>';
 
 require __DIR__ . '/Ufo.php';
 
-$ufo1 = new Ufo;
-$ufo2 = new Ufo;
+$ufo1 = new Ufo('Jupiter', 1, 2, 3);
+$ufo2 = new Ufo('Alfa Centauri', 5547815645646);
 
 
 
+// unset($ufo2);
 
 
-
-$ufo1->speed = 17;
+// $ufo1->speed = 17;
 
 
 // echo $ufo1->speed;
@@ -24,7 +24,15 @@ $ufo1->speed = 17;
 // echo $ufo2->turbo();
 
 echo '<br>';
-echo  $ufo2->whatTimeBig();
+// echo  $ufo2->whatTimeBig();
+
+// $ufo1->destination = 'Mercury';
+
+
+
+echo $ufo1->destination;
+
+// echo $ufo2->bebras;
 
 
 echo '<br>';
